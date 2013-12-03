99deed8536e62b97f64df1a6042a4ebb222369ad
Aileen J Pierce <aileen.pierce@colorado.edu>
2013-12-03 08:49:42 -0700

    week 15

---
+++ task/task/MADViewController.h
@@ -0,0 +1,17 @@
+//
+//  MADViewController.h
+//  task
+//
+//  Created by Aileen Pierce on 11/30/13.
+//  Copyright (c) 2013 Aileen Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *task1;
+@property (weak, nonatomic) IBOutlet UITextField *task2;
+@property (weak, nonatomic) IBOutlet UITextField *task3;
+@property (weak, nonatomic) IBOutlet UITextField *task4;
+
+@end
